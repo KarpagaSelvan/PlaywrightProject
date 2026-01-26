32a33b56c79504cfc49321bd522de93578d3a7e8
KarpagaSelvan <selvankarpaga@gmail.com>
2026-01-26 16:22:24 +0530

    Developed the retry mechanism for the failed test cases using TestNG listeners

--- src/test/java/com/tests/LoginTest2.java
+++ src/test/java/com/tests/LoginTest2.java
@@ -9,8 +9,10 @@ import com.microsoft.playwright.options.AriaRole;
 import com.pages.HomePage;
 import com.pages.LoginPage;
 
+//@Test(retryAnalyzer = com.retry.Retry.class) --> For applying retry to all tests in the class
 public class LoginTest2 extends BaseTest {
 
+//	@Test(retryAnalyzer = com.retry.Retry.class) --> For applying retry to individual test
 	@Test
 	public void loginTest1() {
 
@@ -24,6 +26,7 @@ public class LoginTest2 extends BaseTest {
 		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
 	}
 
+//	@Test(retryAnalyzer = com.retry.Retry.class) --> For applying retry to individual test
 	@Test
 	public void loginTest2() {
 
@@ -31,6 +34,7 @@ public class LoginTest2 extends BaseTest {
 		throw new SkipException("Skipping this test for testng report demo");
 	}
 
+//	@Test(retryAnalyzer = com.retry.Retry.class) --> For applying retry to individual test
 	@Test
 	public void loginTest3() {
 
@@ -47,6 +51,8 @@ public class LoginTest2 extends BaseTest {
 
 	}
 
+//	@Test(retryAnalyzer = com.retry.Retry.class) --> For applying retry to individual test
+	@Test
 	public void loginTest4() {
 
 		// ID locator
@@ -70,6 +76,16 @@ public class LoginTest2 extends BaseTest {
 
 		// xpath locator
 		page.locator("//input[@name='username']").fill("Admin");
+
+		// test data id locator
+		// Using data-test-id attribute
+		page.locator("data-test-id=username").fill("Admin");
+		// Using an inbuilt method in Playwright
+		page.getByTestId("value of test id attribute").click();
+
+		// getByRole locator
+		// Using codegen tool
+		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
 
 	}
 
